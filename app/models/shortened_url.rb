ec2236ff2cc462b34a3f342c321fbb3c889a3e71
David <djmiller1008@yahoo.com>
2021-10-21 09:05:48 +0800

    Added custom validations on shortened_url model

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,6 +1,7 @@
 class ShortenedUrl < ApplicationRecord
     validates :short_url, :long_url, :user_id, presence: true
     validates :short_url, uniqueness: true
+    validate :nonpremium_max, :no_spamming
 
     has_many :taggings,
         primary_key: :id,
@@ -59,6 +60,23 @@ class ShortenedUrl < ApplicationRecord
        end
     end
 
+    def no_spamming
+        last_minute = ShortenedUrl
+          .where('created_at >= ?', 1.minute.ago)
+          .where(user_id: user_id)
+          .length
+    
+        errors[:maximum] << 'of five short urls per minute' if last_minute >= 5
+    end
 
+    def nonpremium_max
+        return if User.find(self.user_id).premium
+        created_urls = ShortenedUrl.where(user_id: user_id)
+            .length
+        
+        if created_urls >= 5
+            errors[:Only] << 'premium users can create more than 5 short urls'
+        end
+    end
 
 end
